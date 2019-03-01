57d65f3212cf2fcfb487b0cfe9a7c57530727742
ValenDesdel <vdestefano@correo.unimet.edu.ve>
2019-03-01 16:20:25 -0400

    Subiendo con rooting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { RecetaComponent } from './receta/receta.component';
+//RUTAS
+import {Routes, RouterModule} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'receta', component: RecetaComponent},
+  {path: 'about', component: AboutComponent}
+]; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    RecetaComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    //RUTAS
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
